844118372f04825a71ba6c47ed97fc09879aba9c
namprobe <166296808+namprobe@users.noreply.github.com>
2024-04-24 05:07:13 +0700

    Update userDAO.java

--- src/java/g2/userTbl/userDAO.java
+++ src/java/g2/userTbl/userDAO.java
@@ -141,6 +141,45 @@ public class userDAO {
         return false;
     }
 
+    public userDTO getUser(String username) throws SQLException, ClassNotFoundException {
+        Connection con = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        userDTO acc = null;
+        try {
+            con = DBUtils.getConnection();
+            if (con != null) {
+                String query = "select * from userTbl where username=?";
+                stm = con.prepareStatement(query);
+                stm.setString(1, username);
+                rs = stm.executeQuery();
+                if (rs.next()) {
+                    int userID = rs.getInt("user_id");
+                    String password = rs.getString("password");
+                    String email = rs.getString("email");
+                    Date birthdate = rs.getDate("birthdate");
+                    boolean isMod = rs.getBoolean("isMod");
+                    boolean isDelete = rs.getBoolean("isDelete");
+                    boolean isBanned = rs.getBoolean("isBanned");
+                    byte[] avatar = rs.getBytes("avatar");
+                    acc = new userDTO(userID, username, password, email, birthdate, isMod, isDelete, isBanned, avatar);
+                }
+            }
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+
+        }
+        return acc;
+    }
+
     public List<userDTO> getUsers(String searchValue)
             throws SQLException, ClassNotFoundException {
         Connection con = null;
@@ -203,24 +242,17 @@ public class userDAO {
         }
         return false;
     }
-    
-    public boolean updateUser(String userName, String password, String email, 
-            Date birthdate, boolean isMod, boolean isBanned, byte[] avatar) throws SQLException, ClassNotFoundException {
+
+    public boolean banUser(String username, boolean isBanned) throws SQLException, ClassNotFoundException {
         Connection con = null;
         PreparedStatement stm = null;
         try {
             con = DBUtils.getConnection();
             if (con != null) {
-                String query = "UPDATE userTbl SET password = ?, email = ?,"
-                        + " birthdate = ?, isMod = ?, isBanned = ?, avatar = ? WHERE username = ?";
+                String query = "UPDATE userTbl SET isBanned = ? WHERE username = ?";
                 stm = con.prepareStatement(query);
-                stm.setString(1, password);
-                stm.setString(2, email);
-                stm.setDate(3, birthdate);
-                stm.setBoolean(4, isMod);
-                stm.setBoolean(5, isBanned);
-                stm.setBytes(6, avatar);
-                stm.setString(7, userName);
+                stm.setBoolean(1, isBanned);
+                stm.setString(2, username);
                 int affectedRows = stm.executeUpdate();
                 return affectedRows == 1;
             }
@@ -235,30 +267,58 @@ public class userDAO {
         return false;
     }
 
-
-    public userDTO getUserByID(int user_id)
+    public boolean updateUser(String userName, String password, String email,
+            Date birthdate, byte[] avatar) throws SQLException, ClassNotFoundException {
+        Connection con = null;
+        PreparedStatement stm = null;
+        try {
+            con = DBUtils.getConnection();
+            if (con != null) {
+                String query = "UPDATE userTbl SET password = ?, email = ?,"
+                        + " birthdate = ?, avatar = ? WHERE username = ?";
+                stm = con.prepareStatement(query);
+                stm.setString(1, password);
+                stm.setString(2, email);
+                stm.setDate(3, birthdate);
+                stm.setBytes(4, avatar);
+                stm.setString(5, userName);
+                int affectedRows = stm.executeUpdate();
+                return affectedRows == 1;
+            }
+        } finally {
+            if (stm != null) {
+                stm.close();
+            }
+            if (con != null) {
+                con.close();
+            }
+        }
+        return false;
+    }
+    
+    public List<userDTO> getMods(boolean isMod)
             throws SQLException, ClassNotFoundException {
         Connection con = null;
         PreparedStatement stm = null;
         ResultSet rs = null;
-        userDTO user = null;
+        List<userDTO> mods = new ArrayList<>();
         try {
             con = DBUtils.getConnection();
             if (con != null) {
-                String query = "SELECT * FROM userTbl WHERE user_id like ?";
+                String query = "SELECT * FROM userTbl WHERE isMod = ?";
                 stm = con.prepareStatement(query);
-                stm.setInt(1,user_id);
+                stm.setBoolean(1,isMod);
                 rs = stm.executeQuery();
-                if (rs.next()) {
+                while (rs.next()) {
                     int userID = rs.getInt("user_id");
-                    String username = rs.getString("username");                 
+                    String username = rs.getString("username");
+                    String password = rs.getString("password");
                     String email = rs.getString("email");
                     Date birthdate = rs.getDate("birthdate");
-                    boolean isMod = rs.getBoolean("isMod");
                     boolean isDelete = rs.getBoolean("isDelete");
                     boolean isBanned = rs.getBoolean("isBanned");
                     byte[] avatar = rs.getBytes("avatar");
-                    user = new userDTO(userID, username, null, email, birthdate, isMod, isDelete, isBanned, avatar);
+                    mods.add(new userDTO(userID, username, password, email, birthdate, isMod, isDelete, isBanned, avatar));
                 }
             }
         } finally {
@@ -272,6 +332,6 @@ public class userDAO {
                 con.close();
             }
         }
-        return user;
+        return mods;
     }
 }
