b502342c11261b71adf5a6b02b022e8f6db776c9
igrrrind <107808162+igrrrind@users.noreply.github.com>
2024-04-23 12:00:26 +0700

    Submit error model

---
+++ src/java/g2/voteTbl/VoteSubmitError.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g2.voteTbl;
+
+/**
+ *
+ * @author admin
+ */
+public class VoteSubmitError {
+    private String VoteHasBeenMadeError;
+    
+   
+    public VoteSubmitError() {
+    }
+
+    public String getVoteHasBeenMadeError() {
+        return VoteHasBeenMadeError;
+    }
+
+    public void setVoteHasBeenMadeError(String VoteHasBeenMadeError) {
+        this.VoteHasBeenMadeError = VoteHasBeenMadeError;
+    }
+    
+      
+}
